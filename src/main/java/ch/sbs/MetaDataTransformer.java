00179f5e3d06d366201cababdcf9932ec90e3e39
Christian Egli <christian.egli@sbs.ch>
2019-05-15 14:11:09 +0200

    Format source

--- src/main/java/ch/sbs/MetaDataTransformer.java
+++ src/main/java/ch/sbs/MetaDataTransformer.java
@@ -27,77 +27,75 @@ import javax.xml.stream.events.XMLEvent;
 import org.apache.commons.io.input.BOMInputStream;
 
 public class MetaDataTransformer {
-    static final String dtb = "http://www.daisy.org/z3986/2005/dtbook/";
-    static final String brl = "http://www.daisy.org/z3986/2009/braille/";
+	static final String dtb = "http://www.daisy.org/z3986/2005/dtbook/";
+	static final String brl = "http://www.daisy.org/z3986/2009/braille/";
 
-    static final QName metaData;
+	static final QName metaData;
 
-    static {
-	metaData = new QName(dtb, "meta");
-    }
-
-    XMLEventFactory eventFactory = XMLEventFactory.newInstance();
-
-    public MetaDataTransformer() {
-    }
-
-    public static void main(String[] args) {
-	if (args.length < 1) {
-	    System.out.println("Usage: Specify XML File Name");
-	    System.exit(1);
+	static {
+		metaData = new QName(dtb, "meta");
 	}
 
-	MetaDataTransformer transformer = new MetaDataTransformer();
+	XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 
-	try {
-	    transformer.transform(new BOMInputStream(new FileInputStream(args[0])), System.out);
-	} catch (Exception e) {
-	    e.printStackTrace();
+	public MetaDataTransformer() {
 	}
-    }
 
-    public void transform(InputStream input, OutputStream output)
-	throws XMLStreamException {
-
-	transform(XMLInputFactory.newInstance().createXMLEventReader(input),
-		  XMLOutputFactory.newInstance().createXMLEventWriter(output));
-    }
-
-    public void transform(Reader input, Writer output)
-	throws XMLStreamException {
-
-	transform(XMLInputFactory.newInstance().createXMLEventReader(input),
-		  XMLOutputFactory.newInstance().createXMLEventWriter(output));
-    }
-
-    public void transform(XMLEventReader reader, XMLEventWriter writer)	throws XMLStreamException {
-
-	boolean insideMetaData = false;
-
-	while (reader.hasNext()) {
-
-	    XMLEvent event = reader.nextEvent();
-
-	    if (event.isStartElement()) {
-		if (event.asStartElement().getName().equals(metaData)) {
-		    insideMetaData = true;
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Usage: Specify XML File Name");
+			System.exit(1);
 		}
-		writer.add(event);
-	    } else if (event.isEndElement()) {
-		if (event.asEndElement().getName().equals(metaData)) {
-		    insideMetaData = false;
+
+		MetaDataTransformer transformer = new MetaDataTransformer();
+
+		try {
+			transformer.transform(new BOMInputStream(new FileInputStream(args[0])), System.out);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		writer.add(event);
-	    } else if (event.isCharacters()) {
-		if (insideMetaData) {
-		    writer.add(eventFactory.createCharacters("foo"));
-		} else {
-		    writer.add(event);
-		}
-	    } else {
-		writer.add(event);
-	    }
 	}
-	writer.flush();
-    }
+
+	public void transform(InputStream input, OutputStream output) throws XMLStreamException {
+
+		transform(XMLInputFactory.newInstance().createXMLEventReader(input),
+				XMLOutputFactory.newInstance().createXMLEventWriter(output));
+	}
+
+	public void transform(Reader input, Writer output) throws XMLStreamException {
+
+		transform(XMLInputFactory.newInstance().createXMLEventReader(input),
+				XMLOutputFactory.newInstance().createXMLEventWriter(output));
+	}
+
+	public void transform(XMLEventReader reader, XMLEventWriter writer) throws XMLStreamException {
+
+		boolean insideMetaData = false;
+
+		while (reader.hasNext()) {
+
+			XMLEvent event = reader.nextEvent();
+
+			if (event.isStartElement()) {
+				if (event.asStartElement().getName().equals(metaData)) {
+					insideMetaData = true;
+				}
+				writer.add(event);
+			} else if (event.isEndElement()) {
+				if (event.asEndElement().getName().equals(metaData)) {
+					insideMetaData = false;
+				}
+				writer.add(event);
+			} else if (event.isCharacters()) {
+				if (insideMetaData) {
+					writer.add(eventFactory.createCharacters("foo"));
+				} else {
+					writer.add(event);
+				}
+			} else {
+				writer.add(event);
+			}
+		}
+		writer.flush();
+	}
 }
