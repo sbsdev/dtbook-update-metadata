e9eabee95b36ec8764674c5c102a3fee6e2866b7
Christian Egli <christian.egli@sbs.ch>
2024-06-26 15:02:37 +0200

    Format source code

--- src/main/java/ch/sbs/MetaDataTransformer.java
+++ src/main/java/ch/sbs/MetaDataTransformer.java
@@ -28,145 +28,142 @@ import org.apache.commons.io.input.BOMInputStream;
  */
 public class MetaDataTransformer {
 
-	/**
-	 * Valid meta data keys.
-	 */
-	public static final String[] keys = { "dc:Title", "dc:Creator", "dc:Subject", "dc:Description", "dc:Publisher",
-			"dc:Date", "dc:Type", "dc:Format", "dc:Identifier", "dc:Source", "dc:Language", "dc:Rights", "dtb:uid",
-			"dtb:sourceEdition", "dtb:sourcePublisher", "dtb:sourceRights", "prod:series", "prod:seriesNumber",
-			"prod:source" };
-	/**
-	 * Valid production meta data keys. If any of those keys are missing in the meta
-	 * data of the input XML they will be added.
-	 */
-	public static final String[] productionKeys = { "prod:series", "prod:seriesNumber", "prod:source" };
+    /**
+     * Valid meta data keys.
+     */
+    public static final String[] keys = { "dc:Title", "dc:Creator", "dc:Subject", "dc:Description", "dc:Publisher",
+					  "dc:Date", "dc:Type", "dc:Format", "dc:Identifier", "dc:Source", "dc:Language", "dc:Rights", "dtb:uid",
+					  "dtb:sourceEdition", "dtb:sourcePublisher", "dtb:sourceRights", "prod:series", "prod:seriesNumber",
+					  "prod:source" };
+    /**
+     * Valid production meta data keys. If any of those keys are missing in the meta
+     * data of the input XML they will be added.
+     */
+    public static final String[] productionKeys = { "prod:series", "prod:seriesNumber", "prod:source" };
 
-	private static final String dtb = "http://www.daisy.org/z3986/2005/dtbook/";
-	private static final String xml = "http://www.w3.org/XML/1998/namespace";
+    private static final String dtb = "http://www.daisy.org/z3986/2005/dtbook/";
+    private static final String xml = "http://www.w3.org/XML/1998/namespace";
 
-	private static final QName head = new QName(dtb, "head");
+    private static final QName head = new QName(dtb, "head");
 
-	private static final QName metaData = new QName(dtb, "meta");
-	private static final QName metaName = new QName("name");
+    private static final QName metaData = new QName(dtb, "meta");
+    private static final QName metaName = new QName("name");
 
-	private static final QName dtbook = new QName(dtb, "dtbook");
-	private static final QName language_attr = new QName(xml, "lang");
+    private static final QName dtbook = new QName(dtb, "dtbook");
+    private static final QName language_attr = new QName(xml, "lang");
 
+    XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+
+    private static void changeMetaAttribute(XMLEventWriter writer, XMLEventFactory eventFactory, String name,
+					    String value) throws XMLStreamException {
+	writer.add(eventFactory.createStartElement("", dtb, "meta"));
+	writer.add(eventFactory.createAttribute("name", name));
+	writer.add(eventFactory.createAttribute("content", value));
+    }
+
+    /**
+     * Transform a given (XML) InputStream to an (XML) OutputStream. If any
+     * properties are given in `env` then update the meta data in the XML
+     * accordingly. The rest of the XML is copied as is. Unlike a normal transformer
+     * DTD entities are not expanded.
+     * 
+     * @param in  an InputStream containing DTBook XML
+     * @param out an OutputStream containing possibly updated DTBook XML
+     * @param env a map of meta data. For any given value the key is updated in the
+     *            DTBook XML
+     * @throws XMLStreamException
+     * 
+     */
+    public static void transform(InputStream in, OutputStream out, Map<String, String> env) throws XMLStreamException {
+	XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+	inputFactory.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, false);
+	inputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+
+	XMLEventReader reader = inputFactory.createXMLEventReader(new BOMInputStream(in));
+	XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(out);
 	XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 
-	private static void changeMetaAttribute(XMLEventWriter writer, XMLEventFactory eventFactory, String name,
-			String value) throws XMLStreamException {
-		writer.add(eventFactory.createStartElement("", dtb, "meta"));
-		writer.add(eventFactory.createAttribute("name", name));
-		writer.add(eventFactory.createAttribute("content", value));
-	}
+	String language = env.get("dc:Language");
 
-	/**
-	 * Transform a given (XML) InputStream to an (XML) OutputStream. If any
-	 * properties are given in `env` then update the meta data in the XML
-	 * accordingly. The rest of the XML is copied as is. Unlike a normal transformer
-	 * DTD entities are not expanded.
-	 * 
-	 * @param in  an InputStream containing DTBook XML
-	 * @param out an OutputStream containing possibly updated DTBook XML
-	 * @param env a map of meta data. For any given value the key is updated in the
-	 *            DTBook XML
-	 * @throws XMLStreamException
-	 * 
-	 */
-	public static void transform(InputStream in, OutputStream out, Map<String,String> env) throws XMLStreamException {
-		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-		inputFactory.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, false);
-		inputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+	Set<String> seen = new HashSet<String>();
 
-		XMLEventReader reader = inputFactory.createXMLEventReader(new BOMInputStream(in));
-		XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(out);
-		XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+	while (reader.hasNext()) {
+	    XMLEvent event = reader.nextEvent();
 
-		String language = env.get("dc:Language");
+	    // update xml:lang attribute
+	    if (language != null && event.isStartElement() && event.asStartElement().getName().equals(dtbook)
+		&& event.asStartElement().getAttributeByName(language_attr) != null
+		&& event.asStartElement().getAttributeByName(language_attr).getValue() != language) {
+		StartElement elem = event.asStartElement();
+		writer.add(eventFactory.createStartElement(dtbook, null, null));
 
-		Set<String> seen = new HashSet<String>();
-
-		while (reader.hasNext()) {
-			XMLEvent event = reader.nextEvent();
-
-			// update xml:lang attribute
-			if (language != null && event.isStartElement() && event.asStartElement().getName().equals(dtbook)
-					&& event.asStartElement().getAttributeByName(language_attr) != null
-					&& event.asStartElement().getAttributeByName(language_attr).getValue() != language) {
-				StartElement elem = event.asStartElement();
-				writer.add(eventFactory.createStartElement(dtbook, null, null));
-
-				Iterator<Namespace> namespaces = elem.getNamespaces();
-				while (namespaces.hasNext()) {
-					Namespace namespace = namespaces.next();
-					writer.add(eventFactory.createNamespace(namespace.getPrefix(), namespace.getNamespaceURI()));
-				}
-				Iterator<Attribute> attributes = elem.getAttributes();
-				while (attributes.hasNext()) {
-					Attribute attribute = attributes.next();
-					QName name = attribute.getName();
-					writer.add(eventFactory.createAttribute(name.getPrefix(), name.getNamespaceURI(),
-							name.getLocalPart(), (name.getLocalPart() == "lang") ? language : attribute.getValue()));
-				}
-			}
-			// update the meta data elements
-			else if (event.isStartElement() && event.asStartElement().getName().equals(metaData)
-					&& event.asStartElement().getAttributeByName(metaName) != null) {
-				String name = event.asStartElement().getAttributeByName(metaName).getValue();
-				seen.add(name);
-
-				boolean newValueGiven = false;
-				for (String entry : keys) {
-					if (name.equals(entry) && env.get(entry) != null) {
-						changeMetaAttribute(writer, eventFactory, name, env.get(entry));
-						newValueGiven = true;
-						break;
-					}
-				}
-				if (!newValueGiven) {
-					writer.add(event);
-				}
-			}
-			// add production meta data if it isn't there yet
-			else if (event.isEndElement() && event.asEndElement().getName().equals(head)) {
-				for (String key : productionKeys) {
-					if (!seen.contains(key)) {
-						writer.add(eventFactory.createStartElement("", dtb, "meta"));
-						writer.add(eventFactory.createAttribute("name", key));
-						writer.add(eventFactory.createAttribute("content", env.getOrDefault(key, "")));
-						writer.add(eventFactory.createEndElement("", dtb, "meta"));
-					}
-				}
-				writer.add(event);
-			}
-			// otherwise just copy the element
-			else {
-				writer.add(event);
-			}
+		Iterator<Namespace> namespaces = elem.getNamespaces();
+		while (namespaces.hasNext()) {
+		    Namespace namespace = namespaces.next();
+		    writer.add(eventFactory.createNamespace(namespace.getPrefix(), namespace.getNamespaceURI()));
 		}
-
-		writer.flush();
-
-	}
-
-	/**
-	 * A tiny xml transformer that will update the meta data in your DTBook xml and
-	 * only that. No entity expansion, no anything! You get your original xml back
-	 * with the updated meta data.
-	 *
-	 * Pass in the xml to be modified on stdin and get the updated xml from stdout.
-	 *
-	 * @param args is ignored
-	 * @throws XMLStreamException
-	 */
-	public static void main(String[] args) throws XMLStreamException {
-	    Properties properties = System.getProperties();
-	    HashMap<String, String> metadata = new HashMap<>();
-	    for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-		metadata.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+		Iterator<Attribute> attributes = elem.getAttributes();
+		while (attributes.hasNext()) {
+		    Attribute attribute = attributes.next();
+		    QName name = attribute.getName();
+		    writer.add(eventFactory.createAttribute(name.getPrefix(), name.getNamespaceURI(),
+							    name.getLocalPart(), (name.getLocalPart() == "lang") ? language : attribute.getValue()));
+		}
 	    }
-	    transform(System.in, System.out, metadata);
-	}
+	    // update the meta data elements
+	    else if (event.isStartElement() && event.asStartElement().getName().equals(metaData)
+		     && event.asStartElement().getAttributeByName(metaName) != null) {
+		String name = event.asStartElement().getAttributeByName(metaName).getValue();
+		seen.add(name);
 
+		boolean newValueGiven = false;
+		for (String entry : keys) {
+		    if (name.equals(entry) && env.get(entry) != null) {
+			changeMetaAttribute(writer, eventFactory, name, env.get(entry));
+			newValueGiven = true;
+			break;
+		    }
+		}
+		if (!newValueGiven) {
+		    writer.add(event);
+		}
+	    }
+	    // add production meta data if it isn't there yet
+	    else if (event.isEndElement() && event.asEndElement().getName().equals(head)) {
+		for (String key : productionKeys) {
+		    if (!seen.contains(key)) {
+			writer.add(eventFactory.createStartElement("", dtb, "meta"));
+			writer.add(eventFactory.createAttribute("name", key));
+			writer.add(eventFactory.createAttribute("content", env.getOrDefault(key, "")));
+			writer.add(eventFactory.createEndElement("", dtb, "meta"));
+		    }
+		}
+		writer.add(event);
+	    }
+	    // otherwise just copy the element
+	    else {
+		writer.add(event);
+	    }
+	}
+	writer.flush();
+    }
+
+    /**
+     * A tiny xml transformer that will update the meta data in your DTBook xml and
+     * only that. No entity expansion, no anything! You get your original xml back
+     * with the updated meta data.
+     *
+     * Pass in the xml to be modified on stdin and get the updated xml from stdout.
+     *
+     * @param args is ignored
+     * @throws XMLStreamException
+     */
+    public static void main(String[] args) throws XMLStreamException {
+	Properties properties = System.getProperties();
+	HashMap<String, String> metadata = new HashMap<>();
+	for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+	    metadata.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+	}
+	transform(System.in, System.out, metadata);
+    }
 }
--- src/test/java/ch/sbs/TransformerTest.java
+++ src/test/java/ch/sbs/TransformerTest.java
@@ -28,133 +28,132 @@ import org.xmlunit.xpath.XPathEngine;
 
 public class TransformerTest {
 
-	private static final String DTBOOK_START = "<!DOCTYPE";
-	private static final String DTBOOK_END = "]>";
+    private static final String DTBOOK_START = "<!DOCTYPE";
+    private static final String DTBOOK_END = "]>";
 
-	private static final String DTBOOK_NAMESPACE_PREFIX = "dtb";
-	private static final String DTBOOK_NAMESPACE_URI = "http://www.daisy.org/z3986/2005/dtbook/";
+    private static final String DTBOOK_NAMESPACE_PREFIX = "dtb";
+    private static final String DTBOOK_NAMESPACE_URI = "http://www.daisy.org/z3986/2005/dtbook/";
 
-	private static final String RESOURCES_XML_TEST_BEFORE_XML = "src/test/resources/xml/before.xml";
-	private static final String RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES = "src/test/resources/xml/before_no_series.xml";
-	private static final String RESOURCES_XML_TEST_AFTER_XML = "src/test/resources/xml/after.xml";
+    private static final String RESOURCES_XML_TEST_BEFORE_XML = "src/test/resources/xml/before.xml";
+    private static final String RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES = "src/test/resources/xml/before_no_series.xml";
+    private static final String RESOURCES_XML_TEST_AFTER_XML = "src/test/resources/xml/after.xml";
 
-	private static final Map<String, String> prefix2Uri = new HashMap<String, String>();
-	private static final HashMap defaultProps = new HashMap();
+    private static final Map<String, String> prefix2Uri = new HashMap<String, String>();
+    private static final HashMap defaultProps = new HashMap();
 
-	static {
-		prefix2Uri.put(DTBOOK_NAMESPACE_PREFIX, DTBOOK_NAMESPACE_URI);
-		defaultProps.put("dtb:sourcePublisher", "a very new Publisher");
-	}
+    static {
+	prefix2Uri.put(DTBOOK_NAMESPACE_PREFIX, DTBOOK_NAMESPACE_URI);
+	defaultProps.put("dtb:sourcePublisher", "a very new Publisher");
+    }
 
-	@Test
-	public void testDoctype() throws XMLStreamException, IOException {
-		String originalContent = new String(Files.readAllBytes(Paths.get(RESOURCES_XML_TEST_BEFORE_XML)),
-				StandardCharsets.UTF_8);
-		// grab only the DOCTYPE
-		String originalDoctype = originalContent.substring(originalContent.indexOf(DTBOOK_START),
-				originalContent.indexOf(DTBOOK_END));
-		String updatedContent = createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML)),
-				defaultProps).toString();
-		String updatedDoctype = updatedContent.substring(updatedContent.indexOf(DTBOOK_START),
-				updatedContent.indexOf(DTBOOK_END));
-		assertEquals(originalDoctype, updatedDoctype);
-	}
+    @Test
+    public void testDoctype() throws XMLStreamException, IOException {
+	String originalContent = new String(Files.readAllBytes(Paths.get(RESOURCES_XML_TEST_BEFORE_XML)),
+					    StandardCharsets.UTF_8);
+	// grab only the DOCTYPE
+	String originalDoctype = originalContent.substring(originalContent.indexOf(DTBOOK_START),
+							   originalContent.indexOf(DTBOOK_END));
+	String updatedContent = createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML)),
+						   defaultProps).toString();
+	String updatedDoctype = updatedContent.substring(updatedContent.indexOf(DTBOOK_START),
+							 updatedContent.indexOf(DTBOOK_END));
+	assertEquals(originalDoctype, updatedDoctype);
+    }
 
-	private ByteArrayOutputStream createTestDocument(InputStream in, Map env)
-			throws XMLStreamException, FileNotFoundException {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		MetaDataTransformer.transform(in, out, env);
-		return out;
-	}
+    private ByteArrayOutputStream createTestDocument(InputStream in, Map env)
+	throws XMLStreamException, FileNotFoundException {
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	MetaDataTransformer.transform(in, out, env);
+	return out;
+    }
 
-	@Test
-	public void testXML() throws FileNotFoundException, XMLStreamException {
-		assertThat(Input.fromStream(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML))),
-				isIdenticalTo(Input.fromStream(new ByteArrayInputStream(
-						createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), defaultProps)
-								.toByteArray()))));
-	}
+    @Test
+    public void testXML() throws FileNotFoundException, XMLStreamException {
+	assertThat(Input.fromStream(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML))),
+		   isIdenticalTo(Input.fromStream(new ByteArrayInputStream(
+									   createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), defaultProps)
+									   .toByteArray()))));
+    }
 
-	@Test
-	public void testEntities() throws FileNotFoundException, XMLStreamException {
-		assertThat(createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML)), defaultProps)
-				.toString(), containsString("&charref"));
-	}
+    @Test
+    public void testEntities() throws FileNotFoundException, XMLStreamException {
+	assertThat(createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_AFTER_XML)), defaultProps)
+		   .toString(), containsString("&charref"));
+    }
 
-	@Test
-	public void testMetadata() throws XMLStreamException, IOException {
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), defaultProps)
-						.toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='dtb:sourcePublisher']/@content", source);
-		assert "a very new Publisher".equals(content);
-	}
+    @Test
+    public void testMetadata() throws XMLStreamException, IOException {
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), defaultProps)
+								  .toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='dtb:sourcePublisher']/@content", source);
+	assert "a very new Publisher".equals(content);
+    }
 
-	@Test
-	public void testProdSeries() throws XMLStreamException, IOException {
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
-						.toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:series']/@content", source);
-		assert "".equals(content);
-	}
+    @Test
+    public void testProdSeries() throws XMLStreamException, IOException {
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
+								  .toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:series']/@content", source);
+	assert "".equals(content);
+    }
 
-	@Test
-	public void testProdSeriesNumber() throws XMLStreamException, IOException {
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
-						.toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:seriesNumber']/@content", source);
-		assert "".equals(content);
-	}
+    @Test
+    public void testProdSeriesNumber() throws XMLStreamException, IOException {
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
+								  .toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:seriesNumber']/@content", source);
+	assert "".equals(content);
+    }
 
-	@Test
-	public void testProdSource() throws XMLStreamException, IOException {
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
-						.toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:source']/@content", source);
-		assert "".equals(content);
-	}
+    @Test
+    public void testProdSource() throws XMLStreamException, IOException {
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML_NO_SERIES)), defaultProps)
+								  .toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String content = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='prod:source']/@content", source);
+	assert "".equals(content);
+    }
 
-	@Test
-	public void testLanguage() throws XMLStreamException, IOException {
-		String expectedLang = "fr";
-		HashMap env = new HashMap();
-		env.put("dc:Language", expectedLang);
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), env).toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String language = xpath.evaluate("/dtb:dtbook/@xml:lang", source);
-		assert expectedLang.equals(language);
-	}
+    @Test
+    public void testLanguage() throws XMLStreamException, IOException {
+	String expectedLang = "fr";
+	HashMap env = new HashMap();
+	env.put("dc:Language", expectedLang);
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), env).toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String language = xpath.evaluate("/dtb:dtbook/@xml:lang", source);
+	assert expectedLang.equals(language);
+    }
 
-	@Test
-	public void testMetaLanguage() throws XMLStreamException, IOException {
-		String expectedLang = "fr";
-		HashMap env = new HashMap();
-		env.put("dc:Language", expectedLang);
-		Source source = Input.fromStream(new ByteArrayInputStream(
-				createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), env).toByteArray()))
-				.build();
-		XPathEngine xpath = new JAXPXPathEngine();
-		xpath.setNamespaceContext(prefix2Uri);
-		String language = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='dc:Language']/@content", source);
-		assert expectedLang.equals(language);
-	}
-
+    @Test
+    public void testMetaLanguage() throws XMLStreamException, IOException {
+	String expectedLang = "fr";
+	HashMap env = new HashMap();
+	env.put("dc:Language", expectedLang);
+	Source source = Input.fromStream(new ByteArrayInputStream(
+								  createTestDocument(new FileInputStream(new File(RESOURCES_XML_TEST_BEFORE_XML)), env).toByteArray()))
+	    .build();
+	XPathEngine xpath = new JAXPXPathEngine();
+	xpath.setNamespaceContext(prefix2Uri);
+	String language = xpath.evaluate("/dtb:dtbook/dtb:head/dtb:meta[@name='dc:Language']/@content", source);
+	assert expectedLang.equals(language);
+    }
 }
